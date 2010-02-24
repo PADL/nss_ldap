7e917c990176e835b4eaa0054d10626925a78342
Luke Howard <lukeh@padl.com>
2010-02-24 11:17:53 +0000

    Darwin compatibility fix

--- irs.h
+++ irs.h
@@ -27,6 +27,9 @@
 #include <sys/types.h>
 #include <netinet/in.h> 
 #include <arpa/nameser.h>
+#ifdef HAVE_ARPA_NAMESER_COMPAT_H
+#include <arpa/nameser_compat.h>
+#endif
 
 #include <grp.h>
 #include <netdb.h>
